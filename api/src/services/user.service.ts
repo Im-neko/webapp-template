e53692e939c669f628dbea5a380532c67b3ae3fb
im-neko <neko@im-neko.net>
2020-03-17 21:04:47 +0900

    保存し忘れてた

--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -21,31 +21,6 @@ class UserService {
     console.log('user', user)
     return signJWT({id: user.id});
   }
-
-  // async saveUser(userInfo: IRegisterUser) {
-  //   const userAuth = await UserAuth.findOne({ where: { email: userInfo.email } });
-  //   if (userAuth) { throw [400, "user already exist."]; }
-  //
-  //   await getManager().transaction(async session => {
-  //
-  //     const user = new User();
-  //     user.first_name = userInfo.first_name;
-  //     user.last_name = userInfo.last_name;
-  //     const savedUser = await session.save(user);
-  //
-  //     const user_id = savedUser.id;
-  //     const userAuth = new UserAuth();
-  //     userAuth.user_id = user_id;
-  //     userAuth.email = userInfo.email;
-  //     const sha512 = createHmac("sha512", HASH_SALT);
-  //     sha512.update(userInfo.password);
-  //     const password = sha512.digest("hex");
-  //     userAuth.password = password;
-  //
-  //     await session.save(userAuth);
-  //   });
-  //   return { email: userInfo.email };
-  // }
 }
 
 export default UserService;
